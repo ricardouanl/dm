a4440479c0d82fc91a9c29a19f3fc79ffcd32c72
ricardouanl <54424491+ricardouanl@users.noreply.github.com>
2022-04-29 04:35:47 -0500

    Create Practica_6.py

---
+++ Practica_6.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import numbers
+import pandas as pd
+from tabulate import tabulate
+
+
+def print_tabulate(df: pd.DataFrame):
+    print(tabulate(df, headers=df.columns, tablefmt="orgtbl"))
+
+def transform_variable(df: pd.DataFrame, x:str)->pd.Series:
+    if isinstance(df[x][0], numbers.Number):
+        return df[x] # type: pd.Series
+    else:
+        return pd.Series([i for i in range(0, len(df[x]))])
+
+
+def linear_regression(df: pd.DataFrame, x:str, y: str)->None:
+    fixed_x = transform_variable(df, x)
+    model= sm.OLS(df[y],sm.add_constant(fixed_x)).fit()
+    print(model.summary())
+
+    coef = pd.read_html(model.summary().tables[1].as_html(),header=0,index_col=0)[0]['coef']
+    df.plot(x=x,y=y, kind='scatter')
+    plt.plot(df[x],[pd.DataFrame.mean(df[y]) for _ in fixed_x.items()], color='green')
+    plt.plot(df1[x],[ coef.values[1] * x + coef.values[0] for _, x in fixed_x.items()], color='red')
+    plt.xticks(rotation=90)
+    plt.savefig(f'img/lr_{y}_{x}.png')
+    plt.close()
+
+
+df1 = pd.read_csv("csv/casos_nl.csv") # type: pd.DataFrame
+print_tabulate(df1.head(5))
+linear_regression(df1, "Fecha", "Casos")
